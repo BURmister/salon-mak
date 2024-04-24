8851dffe3b76dc666a6053e43337f1e492679005
BURmister <olegcernysov903@gmail.com>
2024-04-25 00:06:41 +0300

    some anims

--- main.js
+++ main.js
@@ -1,3 +1,92 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+import { EaselPlugin } from 'gsap/EaselPlugin';
+import { TextPlugin } from 'gsap/TextPlugin';
+
 import './style.scss';
 
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, EaselPlugin, TextPlugin);
+
+   // animation for preview beige section
+   const previewWaveTimeline = gsap.timeline();
+   previewWaveTimeline.from('[data-preview-beigewave]', {
+      y: 'random(-150, -50)',
+      // opacity: 'random(0.6, 0.9)',
+      // ease: 'back.out(2)',
+      duration: 2.5,
+   });
+   previewWaveTimeline.to('[data-preview-beigewave] img', {
+      y: 'random(-50, -35)',
+      objectPosition: `random(0, 100)% 0%`,
+      duration: 4.5,
+      ease: 'back.inOut(1)',
+      repeat: -1,
+      yoyo: true,
+   });
+
+   // parallax animation
+   gsap.to('[data-small-parallax]', {
+      yPercent: 'random(15, 23)',
+      ease: 'none',
+      scrollTrigger: {
+         trigger: '[data-small-parallax]',
+         start: 'top bottom',
+         end: 'bottom top',
+         scrub: true,
+      },
+   });
+
+   // animating text
+   // const animTextList = document.querySelectorAll('[data-text-anim');
+   // if (animTextList && animTextList.length > 0) {
+   //    animTextList.forEach((item) => {
+   //       console.log(item);
+   //       gsap.to(item, {
+   //          scrollTrigger: {
+   //             trigger: item,
+   //             start: 'top bottom',
+   //             end: 'top bottom',
+   //          },
+   //          duration: 1,
+   //          text: item.dataset.textAnim,
+   //          ease: 'power1.out',
+   //       });
+   //    });
+   // }
+
+   // animation for flowers
+   const animFlowerList = document.querySelectorAll('[data-anim-flower]');
+   if (animFlowerList && animFlowerList.length > 0) {
+      animFlowerList.forEach((flower) => {
+         const flowerTimeline = gsap.timeline();
+         flowerTimeline.from(flower, {
+            rotate: 'random(-50, 50, 15)' + 'deg',
+            scale: 'random(0.6, 0.8)',
+            opacity: 'random(0.6, 0.9)',
+            duration: 1.4,
+         });
+         flowerTimeline.to(flower, {
+            x: 'random(-10, 10)',
+            y: 'random(-10, 10)',
+            rotate: 'random(-15, 15, 5)' + 'deg',
+            scale: 'random(0.9, 0.95)',
+            repeat: -1,
+            duration: 4,
+            yoyo: true,
+         });
+
+         gsap.to(flower, {
+            yPercent: 'random(23, 43)',
+            ease: 'none',
+            scrollTrigger: {
+               trigger: flower,
+               start: 'top bottom',
+               end: 'bottom top',
+               scrub: true,
+            },
+         });
+      });
+   }
+});
